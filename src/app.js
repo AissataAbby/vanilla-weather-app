f3d721fe437404d194f3eb245cb36fdf4688f45e
AissataAbby <85876437+AissataAbby@users.noreply.github.com>
2022-06-10 16:18:50 +0200

    added Api key

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-alert = "Hello";
+let apiKey = "90e600906859449d4dbb9a39b1b35129";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=monaco&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
