92cfd4a5d80981019eaba2798cd69c16a1904216
AissataAbby <85876437+AissataAbby@users.noreply.github.com>
2022-06-13 15:06:32 +0200

    Update js day - hour -minutes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,45 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let dateOfTheDayElement = document.querySelector("#dateOfTheDay");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   dateOfTheDayElement.innerHTML = response.data.timezone;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateOfTheDayElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "90e600906859449d4dbb9a39b1b35129";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=monaco&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
+let city = "Monaco";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
