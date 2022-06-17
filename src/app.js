eb5a8a87a117c3881ffca870c00f33deff3afb69
AissataAbby <85876437+AissataAbby@users.noreply.github.com>
2022-06-17 11:53:35 +0200

    Remove Html & js fahrenheit

--- src/app.js
+++ src/app.js
@@ -55,16 +55,7 @@ function handlesubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-
-  alert("hello");
-}
-
 search("Monaco");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handlesubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
